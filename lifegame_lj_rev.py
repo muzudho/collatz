e511feeffd5b76dc14428962257a0e5fad933da3
むずでょ <muzudho1@gmail.com>
2021-07-11 01:26:12 +0900

    7 から 偶数が生えてくんの（＾～＾）？

--- lifegame_lj_rev.py
+++ lifegame_lj_rev.py
@@ -26,38 +26,42 @@ def search(dec, depth, breadth, bi_count):
     # 深さだけインデントしようぜ（＾～＾）？
     indent = ""
     for i in range(0, max_depth-depth):
-        indent += " "
+        indent += "  "
 
     # 表示
     print(f"{indent}({dec}) {bin}")
 
     #print(f"A ({dec}) {dec:b}")
 
-    # 偶数なら 1 引いて 3 で割れるか調べるぜ（＾～＾）
+    if dec == 4:
+        # 4から1に循環するので終わり（＾～＾）
+        return
+
+    # 1 引いて 3 で割れるか調べるぜ（＾～＾）
     if (dec - 1) % 3 == 0:
         # じゃあ 1 引いて 3 で割ったろ（＾～＾）
         dec = (dec - 1 ) // 3
         #print(f"C ({dec}) {dec:b}")
         search(dec=dec, depth=depth-1, breadth=breadth, bi_count = 0)
 
-    else:
+    # 2倍を何回かやるのも試すぜ（＾～＾）
+    if 4 < bi_count:
         # キリが無いので 2倍 するのは終わり（＾～＾）
-        if 4 < bi_count:
-            # 掘るの終わり
-            return
+        # 掘るの終わり
+        return
 
-        # ２倍しようぜ（＾～＾）？
-        for i in range(0, breadth):
-            dec = dec * 2
-            #print(f"D ({dec}) {dec:b}")
+    # ２倍しようぜ（＾～＾）？
+    for i in range(0, breadth):
+        dec = dec * 2
+        #print(f"D ({dec}) {dec:b}")
 
-            search(dec=dec, depth=depth-1, breadth=breadth, bi_count = bi_count+1)
+        search(dec=dec, depth=depth-1, breadth=breadth, bi_count = bi_count+1)
 
     # 掘るの終わり
     return
 
 # 10進数、深さと幅を入力してください。
-print("Please enter a decimal number, depth and breadth. Example: 8 5 2")
+print("Please enter a decimal number, depth and breadth. Example: 8 4 6")
 
 # めんどくさいんで、内部的には10進で計算
 dec, max_depth, breadth = input().split()
