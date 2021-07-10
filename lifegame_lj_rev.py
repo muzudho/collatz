492fe91f04080b620047b0e9e2eeea4612c3ad1b
むずでょ <muzudho1@gmail.com>
2021-07-11 01:53:26 +0900

    重複カット（＾～＾）

--- lifegame_lj_rev.py
+++ lifegame_lj_rev.py
@@ -7,8 +7,18 @@ TODO (2021-07-10 sat) ちゃんと作れてるか分かんない（＾～＾）
 
 numbers = set()
 
+# Return code
+END_NORMAL = 0
+END_DUPURICATE = 1
+END_CUTOFF4 = 2
+END_CUTOFF7 = 3
+END_BI = 4
+END_DEPTH = 5
+
 def search(dec, depth, breadth, bi_count):
     """
+    Parameters
+    ----------
     dec : int
         数
     depth : int
@@ -17,10 +27,20 @@ def search(dec, depth, breadth, bi_count):
         幅
     bi_count : int
         2倍した回数（＾～＾）
+
+    Returns
+    -------
+    int
+        0: 正常終了
+        1: 重複をカット
+        2: '4'をカット
+        3: '7'をカット
+        4: 2倍を終了
+        5: 深さが末端
     """
 
     if depth == 0:
-        return
+        return END_DEPTH
 
     # 深さだけインデントしようぜ（＾～＾）？
     indent = ""
@@ -30,14 +50,14 @@ def search(dec, depth, breadth, bi_count):
     # 重複した枝とか見たくないんで（＾～＾）
     if dec in numbers:
         print(f"{indent}{dec} is a dupricate")
-        return
+        return END_DUPURICATE
 
     numbers.add(dec)
 
     if dec == 4:
         # 4から1に循環するので終わり（＾～＾）
         print(f"{indent}(4) {4:b} is edge case")
-        return
+        return END_CUTOFF4
 
     # 表示
     print(f"{indent}({dec}) {dec:b}")
@@ -50,7 +70,7 @@ def search(dec, depth, breadth, bi_count):
         if dec == 7:
             # なんだかおかしいから抜けよ（＾～＾）
             print(f"{indent}Edge case: 7 is cutoff")
-            return
+            return END_CUTOFF7
 
         # じゃあ 1 引いて 3 で割ったろ（＾～＾）
         dec = (dec - 1 ) // 3
@@ -62,17 +82,20 @@ def search(dec, depth, breadth, bi_count):
     if 4 < bi_count:
         # キリが無いので 2倍 するのは終わり（＾～＾）
         # 掘るの終わり
-        return
+        return END_BI
 
     # ２倍しようぜ（＾～＾）？
     for i in range(0, breadth):
         dec = dec * 2
         #print(f"D ({dec}) {dec:b}")
 
-        search(dec=dec, depth=depth-1, breadth=breadth, bi_count = bi_count+1)
+        ret_code = search(dec=dec, depth=depth-1, breadth=breadth, bi_count = bi_count+1)
+        if ret_code == END_DUPURICATE:
+            # 重複の枝なら、さっさと終わろ（＾～＾）
+            break
 
     # 掘るの終わり
-    return
+    return END_NORMAL
 
 # 10進数、深さと幅を入力してください。
 print("Please enter a decimal number, depth and breadth. Example: 8 7 2")
