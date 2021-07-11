0e86ae99d69d3cfa5e8a8a877dfdf197310d8602
むずでょ <muzudho1@gmail.com>
2021-07-11 16:05:31 +0900

    調整中（＾～＾）

--- lifegame.py
+++ lifegame.py
@@ -1,3 +1,42 @@
+count_str = ""
+bin_str = ""
+dec_str = ""
+# 桁揃えに利用
+count_width = 2
+width = 15
+
+def update_print_number(dec):
+    global count_str
+    global bin_str
+    global dec_str
+    global count_width
+    global width
+    global count
+
+    # めんどくさいんで、２進は文字列で
+    count_str = f"{count}"
+    bin_str = f"{dec:b}"
+
+    # 桁数の更新
+    # めんどくさいんで一気に足すぜ（＾～＾）
+    if count_width < len(count_str):
+        count_width += 5
+    if width < len(bin_str):
+        width += 5
+
+    count_str = count_str.rjust(count_width)
+    bin_str = bin_str.rjust(width)
+    dec_str = str(dec)
+
+    count += 1
+
+def print_number():
+    global count_str
+    global bin_str
+    global dec_str
+
+    print(f"[{count_str}] {bin_str} ({dec})")
+
 # 10進数を入力してください。
 print("Please enter a decimal number. Example: 27")
 
@@ -9,30 +48,15 @@ print(f"Start {dec:b} ({dec})")
 
 # 計算回数
 count = 0
-# 桁揃えに利用
-count_width = 2
-width = 15
 
 while True:
     # 奇数になるまで２で割るぜ（＾～＾）
     while dec % 2 == 0:
         dec = dec // 2
 
-    # めんどくさいんで、２進は文字列で
-    count_str = f"{count}"
-    bin = f"{dec:b}"
-
-    # 桁数の更新
-    # めんどくさいんで一気に足すぜ（＾～＾）
-    if count_width < len(count_str):
-        count_width += 5
-    if width < len(bin):
-        width += 5
-
     # 表示
-    count_str = count_str.rjust(count_width)
-    bin = bin.rjust(width)
-    print(f"[{count_str}] {bin} ({dec})")
+    update_print_number(dec)
+    print_number()
 
     # 1 だったら抜けるぜ（＾～＾）
     if dec==1:
@@ -40,7 +64,5 @@ while True:
 
     # 3x+1 するぜ（＾～＾）
     dec = 3 * dec + 1
-
-    count += 1
 
 print(f"Finished")
