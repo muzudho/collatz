a4b01f6eb16ebc41bd6639b906d1da7b860c280f
むずでょ <muzudho1@gmail.com>
2021-07-11 19:43:42 +0900

    maze

--- maze.py
+++ maze.py
@@ -14,10 +14,6 @@ dec = 1
 # ツリー・パス
 tree_path = [dec]
 
-# 得点
-score = 30
-high_score = score
-
 def is_integer(n):
     try:
         float(n)
@@ -33,13 +29,8 @@ def print_tree_path():
         print(f"{dec}", end="")
         if i<len(tree_path):
             print(f"--", end="")
-    
-    print("")
-    print("")
 
-def print_score():
-    global score
-    print(f"SCORE: {score}")
+    print("")
     print("")
 
 # def print_current(dec):
@@ -55,41 +46,28 @@ def choice_next():
     """次の目的地の表示"""
 
     global dec
-    global score
-    global high_score
 
     while True:
         next_list = [0]
         next_width = 0
-        point_list = [0]
-        point_width = 0
 
         if dec != 4 and dec != 7:
             next = (dec - 1 ) // 3
             if next != 1 and next != 4 and next != 7 and next % 2 == 1:
                 next_list.append(next)
                 next_width = max(next_width, len(str(next)))
-                point = 3 # 奇数は 3 得点
-                point_list.append(point)
-                point_width = max(point_width, len(str(next)))
 
             next = dec * 2
             for i in range(0,10 - len(next_list)):
                 next *= 2
                 next_list.append(next)
                 next_width = max(next_width, len(str(next)))
-                point = -1-1*i # 偶数は減点
-                point_list.append(point)
-                point_width = max(point_width, len(str(next)))
 
         print("Please enter a left number:")
-        next_str = "".rjust(next_width)
-        print(f"   {next_str} Pts.")
         for i, next in enumerate(next_list):
             if i!=0:
                 next_str = str(next_list[i]).rjust(next_width)
-                point_str = str(point_list[i]).rjust(point_width)
-                print(f"{i}: {next_str} {point_str}")
+                print(f"{i}: {next_str}")
 
         print(f"g: goal")
         print("")
@@ -105,15 +83,8 @@ def choice_next():
             if 1 <= choice and choice < len(next_list):
                 dec = next_list[choice]
                 tree_path.append(dec)
-
-                # 得点
-                score += point_list[choice]
-                high_score = max(high_score, score)
-
                 break
 
 while True:
-    print_score()
     print_tree_path()
-    #print_current(dec)
     choice_next()
